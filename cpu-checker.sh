aec3c2ad3bf1e991719aa2b71397667df93c6822
Paul Namuag <paultoytoy@gmail.com>
2019-01-24 23:17:19 +0800

    Clean the cpu-checker

--- cpu-checker.sh
+++ cpu-checker.sh
@@ -3,8 +3,6 @@ tmpfile=$1
 host=$2
 thread=$3
 
-#echo $tmpfile ":" $host ":" $thread ";" > /tmp/igit-paul.txt
-#/usr/bin/ssh -i /home/ubuntu/.ssh/id_rsa ubuntu@${host} "top -p \$(pidof mysqld) -b -n1"|grep '%CPU' -A1|awk 'NR>1{print $9}' >> $tmpfile
 while [ -f $tmpfile ];
 do
     /usr/bin/ssh -i /home/ubuntu/.ssh/id_rsa ubuntu@${host} "sleep 1; top -p \$(pidof mysqld) -b -n1"|grep '%CPU' -A1|awk 'NR>1{print $9}' >> $tmpfile
